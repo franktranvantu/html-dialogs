0da8078d2bb31434abe656fddf2892edd0028c73
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-08 12:53:35 +0700

    Update

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 (function() {
    
-  // Code goes here...
-  
+  var openDialog = document.getElementById('openDialog');
+  var openModalDialog = document.getElementById('openModalDialog');
+  var dialogWindow = document.getElementById('dialogWindow');
+
+  // Open dialog window normally
+  openDialog.addEventListener('click', function () {
+    dialogWindow.show();
+  });
+
+  // Open dialog window as a modal
+  openModalDialog.addEventListener('click', function () {
+    dialogWindow.showModal();
+  })
+
 })();
